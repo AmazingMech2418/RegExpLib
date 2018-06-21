414b049143654b65ba39f6f0e7c7773535be03aa
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-06-21 16:21:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-RegExp.prototype.manipulate = {
-getAll: function() {
-if(this.ignoreCase===true) {
-
-} else {
-Error("Can't find all of anything but /(text)/i");
+Math.factorial = function(arg) {
+    if (arg === 0)
+      { return 1; }
+    else
+      { return arg * Math.factorial( arg - 1 ); }
 }
+Math.toBinary = function(arg) {
+    return (arg >>> 0).toString(2);
+}
+RegExp.prototype.getAll = function() {
+if(this.ignoreCase===true) {
+var bins = [];
+for (var i=0; i<Math.factorial(this.source.length); i++) {
+bins.push(Math.toBinary(i+1));
+}
+} else {
+console.error(Error("Can't find all of anything but /(text)/i"));
+return undefined;
 }
 };
+
